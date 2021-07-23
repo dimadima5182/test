16a67ac2da98c327b608789fa86d65f6cea7ebb7
dimadima5182 <dimadima5182@gmail.com>
2021-07-23 19:09:55 +0300

    add model and app

--- django_test/shop/models.py
+++ django_test/shop/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
-# Create your models here.
+
+class Order(models.Model):
+    number = models.IntegerField()
+    created_date = models.DateTimeField()
+
+    def __str__(self):
+        return self.number
+
+
+class OrderItem(models.Model):
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+    product_name = models.DecimalField(decimal_places=0, max_digits=4)
+    amount = models.IntegerField()
+
+    def __str__(self):
+        return self.product_name
